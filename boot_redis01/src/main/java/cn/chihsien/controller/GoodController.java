859a9b68c1de4f24cf3f7dfaed1a096316742073
chihsiencheng <chihsien_cheng@163.com>
2022-06-11 02:43:37 +0800

    redisson实现分布式锁

--- boot_redis01/src/main/java/cn/chihsien/controller/GoodController.java
+++ boot_redis01/src/main/java/cn/chihsien/controller/GoodController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @describe
- * @auther chihsiencheng
+ * @auther CHkingshin
  */
 @RestController
 public class GoodController {
