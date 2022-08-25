c97300ce7a34368ccf3328268c7eca776f426547
CHkingshin <chihsienkinro@gmail.com>
2022-08-25 14:42:40 +0800

    bitMap

--- redisCache/src/main/java/cn/kingshin/rediscache/service/IUserService.java
+++ redisCache/src/main/java/cn/kingshin/rediscache/service/IUserService.java
@@ -9,16 +9,16 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import javax.servlet.http.HttpSession;
 
 /**
- * <p>
- *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * 用户功能接口
+ * @author KingShin
  */
 public interface IUserService extends IService<User> {
 
     Result sendCode(String phone, HttpSession session);
 
     Result login(LoginFormDTO loginForm, HttpSession session);
+
+    Result sgin();
+
+    Result signCount();
 }
--- redisCache/src/main/java/cn/kingshin/rediscache/service/impl/UserServiceImpl.java
+++ redisCache/src/main/java/cn/kingshin/rediscache/service/impl/UserServiceImpl.java
@@ -12,14 +12,19 @@ import cn.kingshin.rediscache.mapper.UserMapper;
 import cn.kingshin.rediscache.service.IUserService;
 import cn.kingshin.rediscache.utils.RedisConstants;
 import cn.kingshin.rediscache.utils.RegexUtils;
+import cn.kingshin.rediscache.utils.UserHolder;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -98,6 +103,77 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //7.保存用户信息到session中
 
         return Result.ok(token);
+    }
+    /**
+     * @description
+     *        统计用户登录签到信息
+     * @return cn.kingshin.rediscache.dto.Result
+     * @author KingShin
+     * @date 2022/8/25 13:43:06
+     */
+    @Override
+    public Result sgin() {
+        //获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        //获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();//注意是1-31天 不是0-30 要-1
+        //写入redis STEBIT key offset 1
+        stringRedisTemplate.opsForValue().setBit(key,dayOfMonth-1,true);
+        return Result.ok();
+    }
+    /**
+     * @description
+     *        统计连续签到数
+     * @return cn.kingshin.rediscache.dto.Result
+     * @author KingShin
+     * @date 2022/8/25 14:04:49
+     */
+    @Override
+    public Result signCount() {
+        //获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        //获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();//注意是1-31天 不是0-30 要-1
+        //获取本月截至今天为止的签到记录 拿到十进制  BITFIELD sign:5:202208 GET u14 0 查询id为5的用户本月14天的签到次数 从第一天开始查
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+
+        if (result == null || result.isEmpty()) {
+            //没有任何签到结果
+            return Result.ok(0);
+        }
+        //保证健壮性 进行全量判断
+        Long num = result.get(0);
+        if (num == null || num == 0) {
+            return Result.ok(0);
+        }
+        //循环遍历
+        int count = 0;//计数器
+        while (true){
+            //让这个数字和1做与运算 得到数字的最后一个Bit位 || 判断这个bit位是否为0
+            if((num & 1) == 0){
+                //如果为0 未签到
+                break;
+            }else {
+                count++; //如果不为0 已签到 计数器+1
+            }
+            //把数字右移一位 抛弃最后一个bit位 继续下一个bit位
+            num >>>= 1;//先右移一位再赋值给Num 覆盖掉num
+        }
+        return Result.ok(count);
     }
 
     private User createUserWithPhone(String phone) {
